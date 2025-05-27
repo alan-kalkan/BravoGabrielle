6ae16e793c21520421cc0fe1e526e59e10eee137
Alan Kalkan <alan0613@live.fr>
2025-05-28 00:14:03 +0200

    fix

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <>
       <Link href="/home">      <h1 style={{ color: "red", fontSize: "100px", textDecoration: "underline" }}>Hello chouchou, ici screamer dacceuil - clique sur moi</h1>
       </Link>
-      <Link href="/booking"> <h1 style={{ color: "red", fontSize: "100px", textDecoration: "underline" }}>Booking</h1></Link>
       <Image src="/numero.png" alt="Logo" width={1000} height={1000} />
     </>
   )
